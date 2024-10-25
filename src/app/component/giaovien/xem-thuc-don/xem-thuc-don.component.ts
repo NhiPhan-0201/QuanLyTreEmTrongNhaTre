e38534b6773eab230c591d376f360c54942ae26e
Hanh Phuc Cong <hanhphucong28122003@gmail.com>
2024-10-25 22:00:09 +0700

    Cap nhat lai code quanquanlylop, quanlynhomlop

---
+++ src/app/component/giaovien/xem-thuc-don/xem-thuc-don.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-xem-thuc-don',
+  standalone: true,
+  imports: [],
+  templateUrl: './xem-thuc-don.component.html',
+  styleUrl: './xem-thuc-don.component.css'
+})
+export class XemThucDonComponent {
+
+}
---
+++ src/app/component/phuhuynh/xem-danh-gia-tre/xem-danh-gia-tre.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-xem-danh-gia-tre',
+  standalone: true,
+  imports: [],
+  templateUrl: './xem-danh-gia-tre.component.html',
+  styleUrl: './xem-danh-gia-tre.component.css'
+})
+export class XemDanhGiaTreComponent {
+
+}
