64b320f7e186e48b0c2b88fc89e045e7fec043eb
TienNguyen202 <118093161+tiennn3n@users.noreply.github.com>
2024-11-03 16:15:12 +0700

    Xem thong bao chung

---
+++ src/app/component/phuhuynh/xem-thong-bao/xem-thong-bao.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { XemthongbaoService } from '../../APIService/xemthongbao.service';
+
+@Component({
+  selector: 'app-xem-thong-bao',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './xem-thong-bao.component.html',
+  styleUrls: ['./xem-thong-bao.component.css']
+})
+export class XemThongBaoComponent implements OnInit {
+  announcements: any[] = []; // Mảng lưu thông báo
+
+  constructor(private notificationService: XemthongbaoService) {}
+
+  ngOnInit(): void {
+    this.fetchAnnouncements();
+  }
+
+  fetchAnnouncements(): void {
+    this.notificationService.getAllThongbao().subscribe(
+      (data: any[]) => this.announcements = data,
+      (error: any) => console.error('Error fetching announcements:', error)
+    );
+  }
+}
