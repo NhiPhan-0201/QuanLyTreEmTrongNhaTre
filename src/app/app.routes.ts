7db91c73d4bdf30f32f6606925442a132c894f30
hikuh <117068219+hikuh@users.noreply.github.com>
2024-11-03 22:20:22 +0700

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,18 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { FeedbackCategoryComponent } from '../admin/feedback-category/feedback-category.component';
-import { FeedbackComponent } from '../admin/feedback/feedback.component';
+//import { FeedbackComponent } from '../admin/feedback/feedback.component';
 import { SchoolFeeComponent } from '../admin/school-fee/school-fee.component';
 import { NotificationsComponent } from './component/giaovien/notifications/notifications-page.component';
 import { TimetablesComponent } from './component/giaovien/timetable-management/timetable.component';
 import { XemThongBaoComponent } from './component/phuhuynh/xem-thong-bao/xem-thong-bao.component';
+
+
+
+import { FeedbackComponent } from './component/phuhuynh/feedback/feedback.component';
+import { ViewSchoolfeeComponent } from './component/phuhuynh/school-fee/view-schoolfee/view-schoolfee.component';
+import { PayComponent } from './component/phuhuynh/school-fee/pay/pay.component';
+import { ViewComponent } from './component/phuhuynh/roll-call/view/view.component';
 
 import { AdminManagementPageComponent } from '../admin/admin-management-page/admin-management-page.component';
 import { AccountManagementComponent } from '../admin/admin-management-page/account-management/account-management.component';
@@ -26,7 +34,10 @@ export const routes: Routes = [
       { path: 'classes', component: ClassManagementComponent }
     ]
   },
-  {
-    path: 'phuhuynh/xem-thong-bao', component: XemThongBaoComponent
-  }
+  { path: 'phuhuynh/xem-thong-bao', component: XemThongBaoComponent },
+  { path: 'phuhuynh/feedback', component: FeedbackComponent},
+  { path: 'phuhuynh/roll-call', component: ViewComponent},
+  { path: 'phuhuynh/school-fee', component: PayComponent},
+  { path: 'phuhuynh/school-fee', component: ViewSchoolfeeComponent}
+
 ];
