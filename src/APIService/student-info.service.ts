5ffb4f22c13e4dc1cad74cc55b0a505c179a46fc
TienNguyen202 <118093161+tiennn3n@users.noreply.github.com>
2024-11-10 19:53:36 +0700

    Update student-info.service.ts

--- src/APIService/student-info.service.ts
+++ src/APIService/student-info.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ThongTinTre } from '../models/ThongTinTre';
+import { ThongTinPhuHuynh } from '../models/ThongTinPhuHuynh';
 import { ApiResponse } from '../models/ApiResponse.interface';
+
+export interface ChildAndParentInfo {
+  childInfo: ThongTinTre;
+  parentInfo: ThongTinPhuHuynh;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -12,9 +18,10 @@ export class StudentInfoService {
 
   constructor(private http: HttpClient) { }
 
-  getStudentById(id: number): Observable<ApiResponse<ThongTinTre>> {
-    return this.http.get<ApiResponse<ThongTinTre>>(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
+  getStudentAndParentById(id: number): Observable<ApiResponse<ChildAndParentInfo>> {
+    return this.http.get<ApiResponse<ChildAndParentInfo>>(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
   }
+
   private getHeaders(): HttpHeaders {
     const token = typeof window !== 'undefined' ? localStorage.getItem('AccessToken') : null;
     return new HttpHeaders({
