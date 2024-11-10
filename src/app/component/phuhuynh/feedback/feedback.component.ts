8b986057360396dd97c6fc687f326313dbf5efc8
hikuh <117068219+hikuh@users.noreply.github.com>
2024-11-10 10:41:02 +0700

    Update feedback.component.ts

--- src/app/component/phuhuynh/feedback/feedback.component.ts
+++ src/app/component/phuhuynh/feedback/feedback.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
-import { FeedbackService } from '../../../APIService/Fbservice.service';
+import { FeedbackService } from '../../../../APIService/Fbservice.service';
 
 @Component({
   selector: 'app-feedback',
